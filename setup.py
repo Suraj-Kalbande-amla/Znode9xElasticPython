02633e45bfb687922c7f1598005f9008929bca14
Suraj Kalbande <109526212+Suraj-Kalbande-amla@users.noreply.github.com>
2025-04-23 15:59:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='my_script_package',
     version='0.1.0',
-    packages=find_packages(),
+    py_modules=['znodeelastic'],
     install_requires=["elasticsearch", "pandas", "numpy", "json"],
     author='Your Name',
     description='Package for SQL Server external script',
