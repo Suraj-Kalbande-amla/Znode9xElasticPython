1cba207fdba0119440a4fcb9e2ef59bd0a648845
Suraj Kalbande <109526212+Suraj-Kalbande-amla@users.noreply.github.com>
2025-04-23 15:56:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='my_script_package',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=["elasticsearch", "pandas", "numpy", "json"],
     author='Your Name',
     description='Package for SQL Server external script',
 )
